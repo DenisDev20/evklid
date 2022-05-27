15c6ec72d15e7f241bb449ff2524dbccf59de561
DenisDev20 <96378887+DenisDev20@users.noreply.github.com>
2022-05-27 19:51:09 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#header_burger__inner').addEventListener('click', function() {
+document.querySelector('#menu').classList.toggle('is-active')
+    })
+  })
