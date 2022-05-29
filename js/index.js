af18ea18440612948f6873061d56a7068426b5f1
DenisDev20 <96378887+DenisDev20@users.noreply.github.com>
2022-05-29 18:56:37 +0300

    Delete index.js

--- js/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#header_burger__inner').addEventListener('click', function() {
-document.querySelector('#menu').classList.toggle('is-active')
-    })
-  })
