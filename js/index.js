e29531a9fc1b531dbf6033d76e7da8990299eeeb
DenisDev20 <96378887+DenisDev20@users.noreply.github.com>
2022-05-29 18:57:41 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#header_burger__inner').addEventListener('click', function() {
+document.querySelector('#menu').classList.toggle('is-active')
+    })
+  })
+
+
+  // аккордеон
+  $(function () {
+    $("#accordion").accordion();
+  });
+  $(".selector").accordion({
+    classes: {
+      "ui-state-active": "round:hover"
+    }
+  });
+
+  // Свайпер
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      hide: true,
+    },
+
+  });
